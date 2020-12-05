0f91d060a5f7ade2598cc7f1eb87294d384edc3c
Chris Parent <chrisnparent@gmail.com>
2020-12-05 11:09:20 -0500

    day 5 part 1

---
+++ BinarySearch.js
@@ -0,0 +1,13 @@
+let binarySearch = (space = { x: { from: 0, to: 1 }, y: { from: 0, to: 1} }, command = '') => {
+    let dy = Math.round((space.y.to - space.y.from + 1) / 2);
+    let dx = Math.round((space.x.to - space.x.from + 1) / 2);
+    if (command[0] == 'B') space.y.from += dy;
+    else if (command[0] == 'F') space.y.to -= dy;
+    else if (command[0] == 'R') space.x.from += dx;
+    else if (command[0] == 'L') space.x.to -= dx;
+
+    if(space.x.from == space.x.to && space.y.from == space.y.to) 
+        return { x: space.x.from, y: space.y.to };
+
+    return binarySearch(space, command.substring(1));
+}
---
+++ day5/day5_1.js
@@ -0,0 +1,10 @@
+scripts.day5_1 = async () => {
+    await loadScript('BinarySearch.js');
+    let space = () => { return { x: { from: 0, to: 7 }, y: { from: 0, to: 127 } } };
+    let data = (await getFile('day5/day5_1_input.txt'))
+        .split('\r\n')
+        .map(x => binarySearch(space(), x))
+        .map(x => x.y * 8 + x.x);
+
+    terminal.textContent = `Maximum seat id is ${Math.max(...data)}`;
+}
