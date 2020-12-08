75d2089c6a091685faba17e2767731b7bcf534ae
Chris Parent <chrisnparent@gmail.com>
2020-12-07 21:03:23 -0500

    day 7 part 2

--- BagRule.js
+++ BagRule.js
@@ -1,13 +1,23 @@
 class BagRule {
-
     constructor(data = ['']) {
         this.name = `${data[0]} ${data[1]}`;
         this.bags = [];
+        if(data[4] == 'no') return;
+
         for(let i = 4; i < data.length; i += 4) {
             this.bags.push({
                 name: `${data[i+1]} ${data[i+2]}`,
                 count: Number(data[i])
             });
         }
+    }
+
+    contents() {
+        if(this.bags.length == 0) return 1;
+        return this.bags.map(x => x.rule.contents() * x.count).reduce((a,b) => a + b , 0) + 1;
+    }
+
+    link(allrules = [new BagRule()]) {
+        this.bags.forEach(x => x.rule = allrules.find(y => y.name == x.name));
     }
 }
--- day7/day7_2.js
+++ day7/day7_2.js
@@ -2,4 +2,8 @@ scripts.day7_2 = async () => {
     await loadScript('BagRule.js');
     let data = (await getFile('day7/day7_input.txt')).split('\r\n');
     let allrules = data.map(x => new BagRule(x.split(' ')));
+    allrules.forEach(x => x.link(allrules));
+
+    let gold = allrules.find(x => x.name == 'shiny gold');
+    terminal.textContent = `A single gold bag contains ${gold.contents() - 1} other bags`;
 }
