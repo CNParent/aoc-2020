69f4d65b737eb60fac3ffeb9ea514fc104ff79e5
Chris Parent <chrisnparent@gmail.com>
2020-12-13 15:05:31 -0500

    day 14 prep

---
+++ day14/day14_1.js
@@ -0,0 +1,4 @@
+scripts.day14_1 = async () => {
+    let data = (await getFile('day14/input.js')).split('\r\n');
+    terminal.textConent = ``;
+}
---
+++ day14/day14_2.js
@@ -0,0 +1,4 @@
+scripts.day14_2 = async () => {
+    let data = (await getFile('day14/input.js')).split('\r\n');
+    terminal.textConent = ``;
+}
